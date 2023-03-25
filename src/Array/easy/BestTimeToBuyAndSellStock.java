98e70de8e4b05aeae414a78b4d2f3e118de3cafe
Karim Abo Agiza <kemoaboagiza26112000@gmail.com>
2023-03-25 16:15:32 +0200

    adding Best Time To Buy And Sell Stock problem

---
+++ src/Array/easy/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,25 @@
+package Array.easy;
+
+public class BestTimeToBuyAndSellStock {
+
+    /* TODO : d1   d2  d3  d4  d5  d6
+            7   1   5   3   6   7
+    */
+
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                int potentialProfit = price - minPrice;
+                if (potentialProfit > maxProfit) {
+                    maxProfit = potentialProfit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+}
