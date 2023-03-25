bc3e6254a3c690bd0155dbdd7104da423ddc2422
Karim Abo Agiza <kemoaboagiza26112000@gmail.com>
2023-03-25 15:07:18 +0200

    Adding soluation to 2 problems

---
+++ src/easy/FindPivotIndex.java
@@ -0,0 +1,21 @@
+package easy;
+
+public class FindPivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0 , leftSum = 0;
+
+        for(int i =0 ;i<nums.length; i++){
+            totalSum +=nums[i];
+        }
+
+        for(int i =0; i<nums.length; i++){
+            int rightSum = totalSum - leftSum - nums[i];
+            if (rightSum==leftSum)  return i;
+            leftSum+=nums[i];
+
+        }
+        return -1;
+    }
+
+}
---
+++ src/easy/RunningSumOf1dArray.java
@@ -0,0 +1,26 @@
+package easy;
+
+public class RunningSumOf1dArray {
+
+    /***
+     * TODO
+     * Input: nums = [1,2,3,4]
+     * Output: [1,3,6,10]
+     * Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+     */
+
+    public int[] runningSum(int[] nums) {
+        int len = nums.length;
+        int[] prefixSum = new int[len];
+        prefixSum[0] = nums[0];
+
+        for (int i = 1; i < len; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        return prefixSum;
+
+    }
+
+}
+
